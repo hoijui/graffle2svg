7ff1df444a5b42c241db2f9a2264c39f2a383004
Jean-Noël Avila <jn.avila@free.fr>
2010-10-02 23:08:24 +0200

    move from generic assert to assertEqual in unit tests

--- graffle2svg/tests/testRTF.py
+++ graffle2svg/tests/testRTF.py
@@ -7,7 +7,7 @@ class TestValid(TestCase):
     def testSimple(self):
         '''assert extractRTFString(r"{\rtf1\ansi testing}")["string"]=="testing"'''
     for span in extractRTFString(r"{\rtf1\ansi testing}"):
-        assert span["string"]=="testing"
+        self.assertEqual(span["string"],"testing")
 
     def testfontweight(self):
         extractRTFString(r"{\rtf\ansi\fs10\b testing}")
